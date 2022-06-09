2587df62e6bc7d030132e34356b8f0c98503bd02
Daniel <danielkondlatsch.p@gmail.com>
2022-06-08 21:58:01 -0300

    Primeiro protótipo do jogo

---
+++ principal.py
@@ -0,0 +1,29 @@
+print('Select a option')
+
+# if user select 1, print hello, if user select 2, print goodbye
+
+print('1. Iniciar Jogo')
+print('2. Sair')
+print('3. Créditos')
+
+option = input('Enter a number: ')
+
+while True:
+    print('MENU')
+
+    print('1. Iniciar Jogo')
+    print('2. Sair')
+    print('3. Créditos')
+
+    option = input('Enter a number: ')
+
+    if (option == '1'):
+        print('Fod')
+    elif (option == '2'):
+        print('Bye')
+        break
+    elif (option == '3'):
+        print('Allan Narok\nDaniel Kondlatsch')
+    else:
+        print('Opção Inválida')
+        break
